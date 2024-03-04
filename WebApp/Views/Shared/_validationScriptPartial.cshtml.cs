4332e09d2af48bb6104f4dec3a7fa2d9c7325f7b
Ludvig <ludvig.vinoy@hotmail.com>
2024-03-04 13:05:54 +0100

    js added

---
+++ WebApp/Views/Shared/_validationScriptPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Views.Shared
+{
+    public class _validationScriptPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
